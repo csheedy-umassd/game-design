954bed40023b3ce6b07e2d3325282653ed87a4c3
cgibbs98 <cgibbs@umassd.edu>
2019-11-03 00:19:50 -0400

    menu commit

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void playGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void scOpen() {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void exitGame() {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/OptionsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour
+{
+
+    public void goBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
